2415fad2ebdff7c43136ebd45457615d2ba6ea77
hicham <hicham.banny@students.fhnw.ch>
2016-04-27 13:35:50 +0200

    ha jetze mol gluegt

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -10,7 +10,7 @@ public final class App extends Application {
     @Override
     public void start(Stage stage) {
         // Asserts correct project setup (will only compile with Java 8)
-        Supplier<String> helloWorldSupplier = () -> "Hello World";
+        Supplier<String> helloWorldSupplier = () -> "Hel World";
         System.out.println(helloWorldSupplier.get());
     }
 
